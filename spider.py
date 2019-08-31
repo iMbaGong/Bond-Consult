0ea8aff3d1e75aa3fac57e794481e9ba3bf4f8ec
iMbaGong <iMbaGong@adeMacBook-Air.local>
2019-08-31 20:07:29 +0800

    debug spider

--- spider.py
+++ spider.py
@@ -1,33 +1,40 @@
 from selenium import webdriver
+import time
+import json
 
 def main():
-
-    #因为我太菜了不懂怎么爬那个表只能先用selenium了
-    br=webdriver.Chrome()
-    url="http://bond.sse.com.cn/disclosure/info/tb/"
+    # 因为我太菜了不懂怎么爬那个表只能先用selenium了
+    br = webdriver.Chrome()
+    url = "http://bond.sse.com.cn/disclosure/info/tb/#"
     br.get(url)
-    data=[]
-    end=False
+    data = []
+    end = False
+    pages = 0
     while True:
         try:
-            nextPage=br.find_element_by_css_selector("#undefined_next")
+            nextPage = None
+            nextPage = br.find_element_by_class_name("paging_next")
         except:
-            end=True
-
-        tr=br.find_elements_by_tag_name("tr")
-        head=tr[0].text.split(" ")
-        for i in range(len(tr)-1):
-            meta={}
-            line=tr[i].text.split(" ")
-            for j in range(len(line)-1):
-                meta[head[j]]=line[j]
+            end = True
+        pages += 1
+        tr = br.find_elements_by_tag_name("tr")
+        head = tr[0].text.split(" ")
+        for i in range(1, len(tr)):
+            meta = {}
+            line = tr[i].text.split(" ")
+            for j in range(len(line)):
+                meta[head[j]] = line[j]
             data.append(meta)
+            #print(meta)
         if end:
             br.close()
-            break    
+            break
         nextPage.click()
-    
-    print(data)
+        time.sleep(0.2)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(data,f)
+    #print(pages)
+
 
 if __name__ == "__main__":
     main()
