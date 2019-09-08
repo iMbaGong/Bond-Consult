9940bdc52added7d33bf5aa8c4d5cf3f28545e90
iMbaGong <1092467670@qq.com>
2019-09-08 20:37:34 +0800

    add class msg

---
+++ app/src/main/java/com/example/bondconsult/Message.java
@@ -0,0 +1,15 @@
+package com.example.bondconsult;
+
+import java.sql.Date;//TODO:maybe import the wrong one
+import java.util.List;
+
+public class Message {
+
+    private String text;
+    private Date time;
+    private List<myPicture> myPictureList;
+    private User user;
+
+
+
+}
---
+++ app/src/main/java/com/example/bondconsult/User.java
@@ -0,0 +1,5 @@
+package com.example.bondconsult;
+
+public class User {
+    
+}
